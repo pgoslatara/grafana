28d6df7f696973ab13393b53bb8627a84d3ef63d
beejeebus <justin.p.randell@gmail.com>
2026-01-08 18:57:42 +0000

    Not working yet.
    hacky conversion of k8s types to existing types started.

--- public/app/features/datasources/api.ts
+++ public/app/features/datasources/api.ts
@@ -1,12 +1,37 @@
 import { lastValueFrom } from 'rxjs';
 
-import { DataSourceSettings } from '@grafana/data';
+import { config } from '@grafana/runtime';
+import { DataSourceSettings, DataSourceJsonData } from '@grafana/data';
 import { getBackendSrv } from 'app/core/services/backend_srv';
 import { accessControlQueryParam } from 'app/core/utils/accessControl';
 
 export const getDataSources = async (): Promise<DataSourceSettings[]> => {
   return await getBackendSrv().get('/api/datasources');
 };
+
+export interface K8sMetadata {
+  name: string;
+  namespace: string;
+  uid: string;
+  resourceVersion: string;
+  generation: number;
+  creationTimestamp: string;
+  labels: Object,
+  annotations: Object,
+}
+
+export interface DatasourceInstanceK8sSpec {
+  access: string;
+  jsonData: DataSourceJsonData,
+  title: string;
+}
+
+export interface DataSourceSettingsK8s {
+  kind: string;
+  apiVersion: string;
+  metadata: K8sMetadata;
+  spec: DatasourceInstanceK8sSpec;
+}
 
 /**
  * @deprecated Use `getDataSourceByUid` instead.
@@ -28,6 +53,46 @@ export const getDataSourceById = async (id: string) => {
   throw Error(`Could not find data source by ID: "${id}"`);
 };
 
+export const getDataSourceByK8sGroupVersionName = async (k8sGroup: string, k8sVersion: string, k8sName: string, stackId: string) => {
+  console.log("using", k8sGroup, k8sVersion, k8sName, stackId);
+  const response = await lastValueFrom(
+    getBackendSrv().fetch<DataSourceSettingsK8s>({
+      method: 'GET',
+      url: `/apis/${k8sGroup}/${k8sVersion}/namespaces/${stackId}/datasources/${k8sName}`,
+      params: accessControlQueryParam(),
+      showErrorAlert: false,
+    })
+  );
+
+  if (!response.ok) {
+    throw Error(`Could not find data source by group-version-name: "${k8sGroup}" "${k8sVersion}" "${k8sName}"`);
+  }
+  let dsK8sSettings = response.data;
+  let id = parseInt(dsK8sSettings.metadata.labels["grafana.app/deprecatedInternalID"] || "", 10);
+  let dsSettings: DataSourceSettings = {
+    id: id,
+    uid: dsK8sSettings.metadata.name,
+    orgId: 0,
+    name: "",
+    typeLogoUrl: "",
+    type: "",
+    typeName: "",
+    access: "",
+    url: "",
+    user: "",
+    database: "",
+    basicAuth: false,
+    basicAuthUser: "",
+    isDefault: true,
+    jsonData: dsK8sSettings.spec.jsonData,
+    secureJsonFields: {},
+    readOnly: false,
+    withCredentials: false,
+  };
+  console.log(dsSettings);
+  return dsSettings;
+};
+
 export const getDataSourceByUid = async (uid: string) => {
   const response = await lastValueFrom(
     getBackendSrv().fetch<DataSourceSettings>({
@@ -46,6 +111,13 @@ export const getDataSourceByUid = async (uid: string) => {
 };
 
 export const getDataSourceByIdOrUid = async (idOrUid: string) => {
+  if (config.featureToggles.queryServiceWithConnections) {
+    var k8sGroup = "prometheus.datasource.grafana.app";
+    var k8sVersion = "v0alpha1";
+    var stackId = "default";
+    return getDataSourceByK8sGroupVersionName(k8sGroup, k8sVersion, idOrUid, stackId);
+  }
+
   // Try with UID first, as we are trying to migrate to that
   try {
     return await getDataSourceByUid(idOrUid);
